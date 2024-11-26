e84f77ee49f24f54b9266c5e5dff79d02a7af7c5
Harsh Soni <95012191+iam-harshsoni@users.noreply.github.com>
2024-11-26 10:31:56 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
-def call(String Project, String ImageTag, String dockerhubuser, String credentials_id, String usernameVariable, String passVariable){
-  withCredentials([usernamePassword(credentialsId: '${credentials_id}', passwordVariable: '${usernameVariable}', usernameVariable: '${passVariable}')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+def call(String Project, String ImageTag, String dockerhubuser){
+  withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
+      sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
   }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+  sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
 }
